b35a4994ebde5d3f22ec5b577f2319eb899ec677
ShangHun <70267574+Shang-Hun@users.noreply.github.com>
2021-07-10 15:00:12 +0800

    Update main.py

--- cogs/main.py
+++ cogs/main.py
@@ -43,7 +43,7 @@ class Main(commands.Cog):
         owner = await self.bot.fetch_user(617658847552864256)
         ownera = owner.avatar_url
 
-        embed=discord.Embed(title="About Me", description=f"**{self.bot.user}** is a bot with **leveling** and **moderator** functions!",
+        embed=discord.Embed(title="八嘎雅摟", description=f"**{self.bot.user}** is a bot with **leveling** and **moderator** functions!",
          color=0xacacc1)
         embed.set_thumbnail(url=self.bot.user.avatar_url)
         embed.add_field(name="🆔ID", value=self.bot.user.id, inline=True)
